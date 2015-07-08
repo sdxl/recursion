00bce4291f8dc6b2d5a71f150ea5607c9ba41e04
Eric <hoang.e.nguyen@gmail.com>
2015-07-08 17:10:28 -0500

    Came up with initial function and recursive cases. Need to put in a base case.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,72 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  	//determine what the parameter is
+  	var type = typeof(obj);
+  	var finalStr = '';
+
+  	//base case
+  	//returns final str
+
+  	//recursive case
+  	//makes whatever a string, probably cannot use .toString()
+  	
+  
+  	
+ 
+  	
+  	//simple single cases first
+  	
+  	if (type == 'number'){
+  		finalStr+= obj;
+  	};
+  	
+  	if (type == 'string'){ 
+  		finalStr+= "\"" + obj + "\""};
+  	
+  	if (type == 'boolean'){
+  		finalStr += obj};
+  		
+  	//if (type == 'symbol'){
+  		//do nothing
+  	//};
+  	
+  	//if (type == 'undefined'){
+  		//do nothing
+  	//};
+  	
+  	// more complicated case
+  	if (type == 'object'){
+  		type = (Array.isArray(obj) ? 'array' : 'object')
+  	} 
+  	
+  	if (type == 'array'){
+  	//add in the array brackets
+  	finalStr += "[";
+  	//iterate through the array
+  		for(var i=0; i<obj.length; i++){
+  			//call the method on each element of the array
+  			finalStr += stringifyJSON(obj[i]);	
+  		}
+  	// add in the array bracket end
+  		finalStr += "]"
+  	};
+  	
+  	if (type == 'object'){
+  	// add in objects opening curly bracket
+  	finalStr+= "{"
+  		for(var key in object){
+  			//run JSON stringify on key
+  			finalStr+=stringifyJSON(key)
+  			finalStr+= ":"
+  			finalStr+=stringifyJSON(object[key])
+  			finalStr+=","
+  		}
+  			finalStr.slice(0, finalStr.length -1)
+  			finalStr+="}"
+  	};
+
+  	//return str at end of process
+
+  	return finalStr;
 };
